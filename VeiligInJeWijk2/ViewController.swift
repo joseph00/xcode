bd66c9fa5403fc860da5ee0da06293fa1a3ced4f
joseph00 <joepindenkleef@MacBook-Pro-van-Joep.local>
2017-05-22 11:50:32 +0200

    Initial Commit

---
+++ VeiligInJeWijk2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VeiligInJeWijk2
+//
+//  Created by Joep In den Kleef on 22-05-17.
+//  Copyright © 2017 Joep In den Kleef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
